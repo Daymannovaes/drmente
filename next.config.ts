2f74de6a383b02ad8f65214ce08fbc9b0639a670
Dayman Novaes <daymannovaes@gmail.com>
2025-09-27 14:57:47 -0300

    MAJOR: adding next

---
+++ next.config.ts
@@ -0,0 +1,26 @@
+import type { NextConfig } from "next";
+import path from "path";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  turbopack: {
+    root: path.join(__dirname, "./"),
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+};
+
+export default nextConfig;
