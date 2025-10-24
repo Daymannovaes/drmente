131aa801120eb1b460da8e572a59c220abdba9b4
Dayman Novaes <daymannovaes@gmail.com>
2025-10-24 01:50:27 -0300

    command para desligar outros chats

--- src/pages/api/llm-chat/index.ts
+++ src/pages/api/llm-chat/index.ts
@@ -371,9 +371,22 @@ Telefone do paciente: ${senderPhone}
 
 enum COMMANDS {
   RESET = '[command] reset',
+  CHATBOT_ON_GLOBAL = '[command] chatbot global on',
+  CHATBOT_OFF_GLOBAL = '[command] chatbot global off',
   CHATBOT_ON = '[command] chatbot on',
   CHATBOT_OFF = '[command] chatbot off',
+  CHATBOT_ON_OTHER = '[command] chatbot on ',
+  CHATBOT_OFF_OTHER = '[command] chatbot off ',
+  CHATBOT_END_OTHER_instruction = `
+    exemplo:
+    [command] chatbot end (desliga o proprio chat)
+    [command] chatbot end 32 (desliga o chat com id 32)
+`,
   CHATBOT_THRESHOLD_SET = '[command] set threshold ',
+  CHATBOT_THRESHOLD_SET_instruction = `
+    exemplo:
+    [command] set threshold 0.1 (define a probabilidade de 10% para ativar o chatbot)
+`,
   CHATBOT_THRESHOLD_GET = '[command] get threshold',
   SHOW_COMMANDS = '[command] show commands',
 }
@@ -388,14 +401,6 @@ async function handleRequest(payload: WebhookPayload): Promise<{ success: boolea
 
   const redis = await getRedisClient();
 
-  // disable chatbot globally
-  const chatbotActiveGlobally = await redis.get('chatbot_active_globally');
-  if (chatbotActiveGlobally !== '1') {
-    console.log('chatbot not active globally - ignoring');
-    return { success: false, message: "Chatbot is not active globally." };
-  } else {
-    console.log('chatbot active globally - processing');
-  }
 
   // If message is outgoing, ignore it
   if (payload.message_type === 'outgoing') {
@@ -410,6 +415,17 @@ async function handleRequest(payload: WebhookPayload): Promise<{ success: boolea
   if (messageContent === COMMANDS.SHOW_COMMANDS) {
     await sendMessageToChatwoot(process.env.CW_ACCOUNT_ID!, conversationId, "Available commands:\n\n" + Object.values(COMMANDS).join('\n'));
     return { success: true, message: "Commands shown." };
+  }
+
+  if (messageContent === COMMANDS.CHATBOT_ON_GLOBAL) {
+    await redis.set('chatbot_active_globally', '1');
+    await sendMessageToChatwoot(process.env.CW_ACCOUNT_ID!, conversationId, "[command] chatbot global activated.");
+    return { success: true, message: "[command] chatbot global activated." };
+  }
+  if (messageContent === COMMANDS.CHATBOT_OFF_GLOBAL) {
+    await redis.set('chatbot_active_globally', '0');
+    await sendMessageToChatwoot(process.env.CW_ACCOUNT_ID!, conversationId, "[command] chatbot global deactivated.");
+    return { success: true, message: "[command] chatbot global deactivated." };
   }
 
   if (messageContent === COMMANDS.RESET) {
@@ -446,11 +462,43 @@ async function handleRequest(payload: WebhookPayload): Promise<{ success: boolea
     await sendMessageToChatwoot(process.env.CW_ACCOUNT_ID!, conversationId, "[command] chatbot threshold set to " + threshold);
     return { success: true, message: "[command] chatbot threshold set to " + threshold };
   }
+  if (messageContent.startsWith(COMMANDS.CHATBOT_ON_OTHER)) {
+    const conversationToOn = parseInt(messageContent.replace(COMMANDS.CHATBOT_ON_OTHER, ''));
+    console.log('conversationToOn', conversationToOn);
+    if (isNaN(conversationToOn)) {
+      return { success: true, message: "Invalid conversation ID" };
+    }
+    const conversationKey = `chatbot_active:${conversationToOn}`;
+    await redis.set(conversationKey, '1');
+    await sendMessageToChatwoot(process.env.CW_ACCOUNT_ID!, conversationId, "[command] chatbot on - " + conversationToOn);
+    return { success: true, message: "[command] chatbot on - " + conversationToOn };
+  }
 
-  if (messageContent.startsWith(COMMANDS.CHATBOT_THRESHOLD_GET)) {
+  if (messageContent.startsWith(COMMANDS.CHATBOT_OFF_OTHER)) {
+    const conversationToOff = parseInt(messageContent.replace(COMMANDS.CHATBOT_OFF_OTHER, ''));
+    console.log('conversationToOff', conversationToOff);
+    if (isNaN(conversationToOff)) {
+      return { success: true, message: "Invalid conversation ID" };
+    }
+    const conversationKey = `chatbot_active:${conversationToOff}`;
+    await redis.set(conversationKey, '0');
+    await sendMessageToChatwoot(process.env.CW_ACCOUNT_ID!, conversationId, "[command] chatbot off - " + conversationToOff);
+    return { success: true, message: "[command] chatbot off - " + conversationToOff };
+  }
+
+  if (messageContent === COMMANDS.CHATBOT_THRESHOLD_GET) {
     const threshold = await redis.get('chatbot_auto_activation_threshold');
     await sendMessageToChatwoot(process.env.CW_ACCOUNT_ID!, conversationId, "[command] chatbot threshold get: " + threshold);
     return { success: true, message: "[command] chatbot threshold get - " + threshold };
+  }
+
+  // disable chatbot globally
+  const chatbotActiveGlobally = await redis.get('chatbot_active_globally');
+  if (chatbotActiveGlobally !== '1') {
+    console.log('chatbot not active globally - ignoring');
+    return { success: false, message: "Chatbot is not active globally." };
+  } else {
+    console.log('chatbot active globally - processing');
   }
 
   // Check if chatbot is active
